d14757503a5416e917a16a9494b3da322ec82277
Feng Chang <fchang@bushelpowered.com>
2023-03-29 23:33:15 -0500

    update my first getWeatherInfo test with more mock data fields

--- MyWeatherTests/WeatherServiceTest.swift
+++ MyWeatherTests/WeatherServiceTest.swift
@@ -68,8 +68,28 @@ final class WeatherServiceTest: XCTestCase {
         let coordinate2D = CLLocationCoordinate2D(latitude: 44.34, longitude: 10.99)
         weatherService.getWeatherInfo(with: coordinate2D) { result in
             switch result {
-            case .success(let weather):
-                XCTAssertEqual(weather.base, "stations", "Incorrect userID.")
+            case .success(let weatherInfo):
+                XCTAssertEqual(weatherInfo.coord.lon, 10.99, "Incorrect lat.")
+                XCTAssertEqual(weatherInfo.coord.lat, 44.34, "Incorrect lon.")
+                XCTAssertEqual(weatherInfo.weather.first?.main, "Rain", "Incorrect main.")
+                XCTAssertEqual(weatherInfo.weather.first?.id, 501, "Incorrect id.")
+                XCTAssertEqual(weatherInfo.weather.first?.description, "moderate rain", "Incorrect description.")
+                XCTAssertEqual(weatherInfo.weather.first?.icon, "10d", "Incorrect icon.")
+                XCTAssertEqual(weatherInfo.base, "stations", "Incorrect userID.")
+                XCTAssertEqual(weatherInfo.main.temp, 298.48, "Incorrect temp.")
+                XCTAssertEqual(weatherInfo.main.feelsLike, 298.74, "Incorrect feelsLike.")
+                XCTAssertEqual(weatherInfo.main.tempMin, 297.56, "Incorrect tempMin.")
+                XCTAssertEqual(weatherInfo.main.tempMax, 300.05, "Incorrect tempMax.")
+                XCTAssertEqual(weatherInfo.main.pressure, 1015, "Incorrect pressure.")
+                XCTAssertEqual(weatherInfo.main.humidity, 64, "Incorrect humidity.")
+                XCTAssertEqual(weatherInfo.main.seaLevel, 1015, "Incorrect seaLevel.")
+                XCTAssertEqual(weatherInfo.main.grndLevel, 933, "Incorrect grndLevel.")
+                XCTAssertEqual(weatherInfo.visibility, 10000, "Incorrect visibility.")
+                XCTAssertEqual(weatherInfo.wind.speed, 0.62, "Incorrect wind speed.")
+                XCTAssertEqual(weatherInfo.wind.deg, 349, "Incorrect wind deg.")
+                XCTAssertEqual(weatherInfo.wind.gust, 1.18, "Incorrect wind gust.")
+                XCTAssertEqual(weatherInfo.rain.oneHour, 3.16, "Incorrect rain oneHour.")
+                XCTAssertEqual(weatherInfo.clouds.all, 100, "Incorrect clouds all.")
             case .failure(let error):
                 XCTFail("Unexpected error: \(error.localizedDescription)")
             }
